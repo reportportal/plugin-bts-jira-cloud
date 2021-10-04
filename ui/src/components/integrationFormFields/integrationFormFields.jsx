76265cd44b4a0f10dc2736ac19c27094e00ccf9e
Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>
2021-10-04 17:55:20 +0300

    EPMRPP-66091 - add intergation form

---
+++ ui/src/components/integrationFormFields/integrationFormFields.jsx
@@ -0,0 +1,75 @@
+export const IntegrationFormFields = (props) => {
+  const { initialize, disabled, lineAlign, initialData, ...extensionProps } = props;
+  const {
+    lib: { React },
+    components: { IntegrationFormField, FieldErrorHint, Input, InputTextArea },
+    validators: { requiredField, btsUrl, btsProject, btsIntegrationName },
+  } = extensionProps;
+
+  return (
+    <>
+      <IntegrationFormField
+        name="integrationName"
+        disabled={disabled}
+        label="Integration Name"
+        required
+        maxLength="55"
+        validate={btsIntegrationName}
+        lineAlign={lineAlign}
+      >
+        <FieldErrorHint>
+          <Input mobileDisabled />
+        </FieldErrorHint>
+      </IntegrationFormField>
+      <IntegrationFormField
+        name="url"
+        disabled={disabled}
+        label="Link to BTS"
+        required
+        validate={btsUrl}
+        lineAlign={lineAlign}
+      >
+        <FieldErrorHint>
+          <Input mobileDisabled />
+        </FieldErrorHint>
+      </IntegrationFormField>
+      <IntegrationFormField
+        name="project"
+        disabled={disabled}
+        label="Project name in BTS"
+        required
+        maxLength="80"
+        validate={btsProject}
+        lineAlign={lineAlign}
+      >
+        <FieldErrorHint>
+          <Input mobileDisabled />
+        </FieldErrorHint>
+      </IntegrationFormField>
+      <IntegrationFormField
+        name="email"
+        disabled={disabled}
+        label="Login or e-mail"
+        required
+        lineAlign={lineAlign}
+        validate={requiredField}
+      >
+        <FieldErrorHint>
+          <Input mobileDisabled />
+        </FieldErrorHint>
+      </IntegrationFormField>
+      <IntegrationFormField
+        name="apiToken"
+        label="API Token"
+        required
+        disabled={disabled}
+        lineAlign={lineAlign}
+        validate={requiredField}
+      >
+        <FieldErrorHint>
+          <InputTextArea type="text" mobileDisabled />
+        </FieldErrorHint>
+      </IntegrationFormField>
+    </>
+  );
+};
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,4 +1,19 @@
+import { IntegrationFormFields, IntegrationSettings } from 'components';
+
 window.RP.registerPlugin({
   name: 'JIRA Cloud',
-  extensions: [],
+  extensions: [
+    {
+      name: 'integrationFormFields',
+      title: 'JIRA Cloud plugin fields',
+      type: 'uiExtension:integrationFormFields',
+      component: IntegrationFormFields,
+    },
+    {
+      name: 'integrationSettings',
+      title: 'JIRA Cloud plugin settings',
+      type: 'uiExtension:integrationSettings',
+      component: IntegrationSettings,
+    },
+  ],
 });
