199d23d76ed9ff4b7c792327214b3377819dc60a
Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>
2021-10-21 12:07:38 +0300

    EPMRPP-67644 - rename project field

--- ui/src/components/constans.js
+++ ui/src/components/constans.js
@@ -1,7 +1,7 @@
 export const LABELS = {
   INTEGRATION_NAME: 'Integration Name',
   URL: 'Link to BTS',
-  PROJECT: 'Project name in BTS',
+  PROJECT: 'Project key in BTS',
   EMAIl: 'Email',
   TOKEN: 'API Token',
   AUTH_BY: 'Authorized by email',
