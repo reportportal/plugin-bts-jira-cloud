6fdb067568768db8e8e2a6895d2c75c4f1a1ce17
Darragh Grace <darra.grace@gmail.com>
2026-07-07 15:37:25 +0100

    Fix allowed url pattern to allow urls with /jira as per Atlassian documentation - acme.atlassian.net/jira - See https://github.com/reportportal/plugin-bts-jira-cloud/issues/121 (#128)

--- src/main/java/com/epam/reportportal/extension/jira/utils/IntegrationValidator.java
+++ src/main/java/com/epam/reportportal/extension/jira/utils/IntegrationValidator.java
@@ -28,7 +28,7 @@ import java.util.regex.Pattern;
  */
 public final class IntegrationValidator {
 
-  private static final String JIRA_URL_PATTERN = "https://[^?]*\\.(atlassian\\.(com|net)|jira\\.com)/?$";
+  private static final String JIRA_URL_PATTERN = "https://[^?]*\\.(atlassian\\.(com|net)|jira\\.com)(/jira)?/?$";
 
   private IntegrationValidator() {
     //static only
